f16228a7a7bdbd2b127b87cb35e0629b0502b71e
Alessandro Giunta <alegiunt99@gmail.com>
2022-06-10 00:57:04 +0200

    editing codes

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,1 +1,2 @@
+@extends('layouts.front-app')
 
--- resources/views/layouts/front-app.blade.php
+++ resources/views/layouts/front-app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('front-app.js') }}" defer></script>
+    <script src="front-app.js" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
