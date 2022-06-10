cc9a3efb453bc1a70b453a28e6345e7006557be6
Alessandro Giunta <alegiunt99@gmail.com>
2022-06-10 17:45:46 +0200

    Add Header and Links

--- resources/js/app/App.vue
+++ resources/js/app/App.vue
@@ -1,10 +1,26 @@
 <template>
+<div>
+  <!-- Header con i links -->
+  <HeaderComponent />
+
+  <!-- contenuto che varia -->
   <router-view></router-view>
+
+</div>
+  
 </template>
 
 <script>
+import HeaderComponent from '../components/HeaderComponent'
+
 export default {
     name: 'App',
+
+    components: {
+    
+      HeaderComponent
+        
+    }
 }
 </script>
 
---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="header-nav">
+    <div class="link-h">
+        <router-link :to="{ name: 'home' }">Home</router-link>
+    </div>
+
+    <div class="link-h">
+        <router-link :to="{ name: 'blog' }">Blog</router-link>
+    </div>
+      
+  </div>
+</template>
+
+<script>
+	export default {
+    		name: 'HeaderComponent',
+	}
+</script>
+
+<style lang="scss" scoped>
+.header-nav{
+    display: flex;
+    column-gap: 10px;
+    padding: 15px;
+    background-color: lightgray;
+}
+</style>
